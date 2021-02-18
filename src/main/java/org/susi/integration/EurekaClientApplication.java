28240342620029f523a57200115e9095c8aced8c
Susinda Perera <susinda@gmail.com>
2021-02-18 10:16:00 +0530

    Update EurekaClientApplication.java
    Test change

--- src/main/java/org/susi/integration/EurekaClientApplication.java
+++ src/main/java/org/susi/integration/EurekaClientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.integration.http.config.EnableIntegrationGraphController;
 
+
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableIntegrationGraphController(allowedOrigins = "*")
